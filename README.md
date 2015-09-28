81fb96369daf382485b7df17733fd1c6c3f4dbeb
omaryahir <omaryahir@gmail.com>
2015-09-28 16:46:21 -0500

    ADD: Pomodoro

---
+++ README.md
@@ -0,0 +1,5 @@
+
+references:
+http://askubuntu.com/questions/277215/make-a-sound-once-process-is-complete
+
+
---
+++ count20.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from os import system
+
+a = datetime.today() + timedelta(minutes=20)
+continuar = True;
+
+seconds = 0
+diff = 0
+while continuar:
+    b = datetime.today()
+    if b >= a:
+        msg="Ready, I count 20 minutes."
+        command="%s %s" % ("spd-say",msg)
+        system(command)
+    else: 
+        #system("clear")
+        diff = a - b
+        minutes = (diff.seconds + diff.microseconds / 1000000) / 60
+
+        if seconds != diff.seconds:
+            seconds = diff.seconds
+            msg = "%s" % diff
+            system("clear")
+            print  " Counting ...     %s" % msg[0:-7]
+            #print "%s" % (minutes)
+    
+
+    
+
+
+
